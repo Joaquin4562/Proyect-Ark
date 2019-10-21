462a9eda4333ef61c31da2dbc90646ac3cc996bd
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2019-10-21 13:28:01 -0500

    Agrege modificaciones de pantalla de comparar

---
+++ app/src/main/java/com/example/arkk/animales2.java
@@ -0,0 +1,36 @@
+package com.example.arkk;
+
+public class animales2 {
+    private int id;
+    private String nombre;
+
+    public animales2(){
+
+    }
+
+    public animales2(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
+}
--- app/src/main/java/com/example/arkk/pantallaComparar.java
+++ app/src/main/java/com/example/arkk/pantallaComparar.java
@@ -2,11 +2,15 @@ package com.example.arkk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,16 +18,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class pantallaComparar extends AppCompatActivity {
     TextView raza,longevidad, temax, temin, pesprom, lechee, crias, periodo, calidad;
-    TextView raza2,longevidad2, temax2, temin2, pesprom2, leche2, crias2, periodo2, calidad2;
+    TextView raza2,longevidad2, temax2, temin2, pesprom2, leche2, crias2, periodo2, calidad2,name;
     int posicion;
     private RadioButton rr1, rr2, rr3,rr4,rr5,rr6;
     String nombre1, nombre2;
+    SearchView se,se2;
+    ListView list,list2;
+    ArrayList<animales> lista;
+    ArrayList<animales2> lista2;
+    ArrayAdapter<animales> adapter;
+    ArrayAdapter<animales2> adapter2;
+    String v;
 
-
-
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,20 +58,119 @@ public class pantallaComparar extends AppCompatActivity {
         this.crias2=findViewById(R.id.crias2);
         periodo2=findViewById(R.id.periodoGestacion2);
         calidad2=findViewById(R.id.calidadLeche2);
-        rr1=findViewById(R.id.r1);
-        rr2=findViewById(R.id.r2);
-        rr3=findViewById(R.id.r3);
-        rr4=findViewById(R.id.radioButton4);
-        rr5=findViewById(R.id.radioButton5);
-        rr6=findViewById(R.id.radioButton6);
+
+        se=findViewById(R.id.search);
+        list=findViewById(R.id.lis);
+        se2=findViewById(R.id.search2);
+        list2=findViewById(R.id.lis2);
+
+        lista = new ArrayList<animales>();
+        lista.add(new animales(1,"Simbrah"));
+        lista.add(new animales(2,"Pardo suiza"));
+        lista.add(new animales(3,"Brahman"));
+
+        lista2 = new ArrayList<animales2>();
+        lista2.add(new animales2(1,"Simbrah"));
+        lista2.add(new animales2(2,"Pardo suiza"));
+        lista2.add(new animales2(3,"Brahman"));
 
 
+        adapter = new ArrayAdapter<animales>(this, android.R.layout.simple_list_item_1, lista);
+        list.setAdapter(adapter);
+
+        adapter2 = new ArrayAdapter<animales2>(this, android.R.layout.simple_list_item_1, lista2);
+        list2.setAdapter(adapter2);
+
+        se.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String s){
+
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s){
+                adapter.getFilter().filter(s);
+
+                return false;
+            }
+        });
+
+        se2.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String s){
+
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s){
+                adapter2.getFilter().filter(s);
+
+                return false;
+            }
+        });
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                animales o=lista.get(i);
+                v=o.getNombre();
+                try {
+                    Statement st = conexionBd().createStatement();
+                    ResultSet rs = st.executeQuery("Select Raza,Peso_Promedio,Longevidad,Temperatura_maxima,Temperatura_minima," +
+                            "Litros_de_leche_al_año, Crias_en_vida_productiva, Periodo_de_gestacion, Calidad_de_leche from Vacas where Raza ='" + o.getNombre()+"'");
+                    while (rs.next()) {
+                        raza.setText(raza.getText().toString()+"    "+rs.getString("Raza"));
+                        pesprom.setText(pesprom.getText().toString()+"    "+rs.getString("Peso_Promedio"));
+                        longevidad.setText(longevidad.getText().toString()+"    "+rs.getString("Longevidad"));
+                        temax.setText(temax.getText().toString()+"    "+rs.getString("Temperatura_maxima"));
+                        temin.setText(temin.getText().toString()+"    "+rs.getString("Temperatura_minima"));
+                        lechee.setText(lechee.getText().toString()+"    "+rs.getString("Litros_de_leche_al_año"));
+                        crias.setText(crias.getText().toString()+"    "+rs.getString("Crias_en_vida_productiva"));
+                        periodo.setText(periodo.getText().toString()+"    "+rs.getString("Periodo_de_gestacion"));
+                        calidad.setText(calidad.getText().toString()+"    "+rs.getString("Calidad_de_leche"));
+
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(pantallaComparar.this, e.getMessage(), Toast.LENGTH_LONG).show();
+
+                }
+                Toast.makeText(pantallaComparar.this,"nombre "+ o.getNombre(), Toast.LENGTH_LONG).show();
+
+            }
+        });
 
 
+        list2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                animales2 o=lista2.get(i);
+                v=o.getNombre();
+                try {
+                    Statement st = conexionBd().createStatement();
+                    ResultSet rs = st.executeQuery("Select Raza,Peso_Promedio,Longevidad,Temperatura_maxima,Temperatura_minima," +
+                            "Litros_de_leche_al_año, Crias_en_vida_productiva, Periodo_de_gestacion, Calidad_de_leche from Vacas where Raza ='" + o.getNombre()+"'");
+                    while (rs.next()) {
+                        raza2.setText(raza2.getText().toString()+"    "+rs.getString("Raza"));
+                        pesprom2.setText(pesprom2.getText().toString()+"    "+rs.getString("Peso_Promedio"));
+                        longevidad2.setText(longevidad2.getText().toString()+"    "+rs.getString("Longevidad"));
+                        temax2.setText(temax2.getText().toString()+"    "+rs.getString("Temperatura_maxima"));
+                        temin2.setText(temin2.getText().toString()+"    "+rs.getString("Temperatura_minima"));
+                        leche2.setText(leche2.getText().toString()+"    "+rs.getString("Litros_de_leche_al_año"));
+                        crias2.setText(crias2.getText().toString()+"    "+rs.getString("Crias_en_vida_productiva"));
+                        periodo2.setText(periodo2.getText().toString()+"    "+rs.getString("Periodo_de_gestacion"));
+                        calidad2.setText(calidad2.getText().toString()+"    "+rs.getString("Calidad_de_leche"));
 
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(pantallaComparar.this, e.getMessage(), Toast.LENGTH_LONG).show();
+
+                }
+                Toast.makeText(pantallaComparar.this,"nombre "+ o.getNombre(), Toast.LENGTH_LONG).show();
+
+            }
+        });
 
     }
-
 
 
 
@@ -74,31 +184,29 @@ public class pantallaComparar extends AppCompatActivity {
 
 
    public void ObtenerVaca(View v){
+                try {
+                    Statement st = conexionBd().createStatement();
+                    ResultSet rs = st.executeQuery("Select Raza,Peso_Promedio,Longevidad,Temperatura_maxima,Temperatura_minima," +
+                            "Litros_de_leche_al_año, Crias_en_vida_productiva, Periodo_de_gestacion, Calidad_de_leche from Vacas where Raza ='Simbrah'");
+                    while (rs.next()) {
+                        raza.setText(raza.getText().toString()+"    "+rs.getString("Raza"));
+                        pesprom.setText(pesprom.getText().toString()+"    "+rs.getString("Peso_Promedio"));
+                        longevidad.setText(longevidad.getText().toString()+"    "+rs.getString("Longevidad"));
+                        temax.setText(temax.getText().toString()+"    "+rs.getString("Temperatura_maxima"));
+                        temin.setText(temin.getText().toString()+"    "+rs.getString("Temperatura_minima"));
+                        lechee.setText(lechee.getText().toString()+"    "+rs.getString("Litros_de_leche_al_año"));
+                        crias.setText(crias.getText().toString()+"    "+rs.getString("Crias_en_vida_productiva"));
+                        periodo.setText(periodo.getText().toString()+"    "+rs.getString("Periodo_de_gestacion"));
+                        calidad.setText(calidad.getText().toString()+"    "+rs.getString("Calidad_de_leche"));
 
-            try {
-                Statement st = conexionBd().createStatement();
-                ResultSet rs = st.executeQuery("Select Raza,Peso_Promedio,Longevidad,Temperatura_maxima,Temperatura_minima," +
-                        "Litros_de_leche_al_año, Crias_en_vida_productiva, Periodo_de_gestacion, Calidad_de_leche from Vacas where Raza='Brahman'");
-                while (rs.next()) {
-                    raza.setText(raza.getText().toString()+"    "+rs.getString("Raza"));
-                    pesprom.setText(pesprom.getText().toString()+"    "+rs.getString("Peso_Promedio"));
-                    longevidad.setText(longevidad.getText().toString()+"    "+rs.getString("Longevidad"));
-                    temax.setText(temax.getText().toString()+"    "+rs.getString("Temperatura_maxima"));
-                    temin.setText(temin.getText().toString()+"    "+rs.getString("Temperatura_minima"));
-                    lechee.setText(lechee.getText().toString()+"    "+rs.getString("Litros_de_leche_al_año"));
-                    crias.setText(crias.getText().toString()+"    "+rs.getString("Crias_en_vida_productiva"));
-                    periodo.setText(periodo.getText().toString()+"    "+rs.getString("Periodo_de_gestacion"));
-                    calidad.setText(calidad.getText().toString()+"    "+rs.getString("Calidad_de_leche"));
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
 
                 }
-            } catch (Exception e) {
-                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-
-            }
-
-
 
     }
+
 
     public void ObtenerVacaPardo(View v){
 
@@ -247,4 +355,16 @@ public class pantallaComparar extends AppCompatActivity {
         }
         return conexion;
     }
-}
+
+
+
+
+        {
+
+        }
+
+
+        }
+
+
+
